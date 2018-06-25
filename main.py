3ca93ec52f6b309ae66b7e325733344c5f2c59f9
chingyi <tadpole655010@gmail.com>
2018-06-25 09:12:45 +0800

    Support 2^b

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from BCJR import BCJR
 from util import *
 
-p_mat = read_mat('par.csv')
-bcjr = BCJR(p_mat)
+p_mat = read_mat('file3.csv')
+bcjr = BCJR(p_mat, b=2)
 bcjr.plot_sections(0,5)
--- util.py
+++ util.py
@@ -5,31 +5,34 @@ import networkx as nx
 
 zero = np.zeros(2, dtype=int)
 
-def vex_in_edg( vex, edg ):
+def vex_eq( v0, v1 ):
+    print("Equal: ", v0, v1, np.array_equal(v0, v1))
+    return np.array_equal(v0, v1)
+
+def vex_of_edg( vex, edg ):
     edgs = []
+    # print("vex = ", vex)
     for v in vex:
         for e in edg:
             v0, a, v1 = e
-            if np.array_equal(v0,v):
+            # print("e = ", e)
+            if vex_eq(v0,v):
                 edgs.append(v)
                 break
     return edgs
 
 def array2str( in_arr, i ):
-    return str(i) + '_' + str(in_arr[0]) + str(in_arr[1])
-
-def int2bistr( i, dim ):
-    bistr = ""
-    input_int = i
-    for nbit in range(0,dim):
-        bistr = str(i%2) + bistr
-        i = int(i/2)
-    return bistr
+    name = str(i) + '_'
+    for s in in_arr:
+        name += str(s)
+    return name
 
 def arr2bistr( arr, dim ):
     bistr = ""
-    for digit in arr:
-        bistr = bistr + str(digit)
+    for symbol in arr:
+        for digit in symbol.value:
+            bistr = bistr + str(int(digit))
+    # print('bistr = ', bistr)
     return bistr
 
 def in_list( vec_list, target ):
@@ -41,7 +44,20 @@ def read_mat( filename ):
     with open( filename, newline='') as f:
         reader = csv.reader(f)
         rows = []
+        symbols = []
         for row in reader:
-            rows.append([int(x) for x in row])
-        p_mat = np.array(rows)
-    return p_mat
+            rows = []
+            for symbol in row:
+                symbol_str = []
+                for gf in symbol:
+                    symbol_str.append(int(gf))
+                print("symbol = ", symbol_str)
+                rows.append(symbol_str)
+            symbols.append(rows)
+        print("symbols = ", symbols)
+        rows_nparr = np.swapaxes(np.array(symbols),0,1)
+        print("rows_nparr = ", rows_nparr.shape)
+        size = int(int(rows_nparr.size)/len(symbols))
+        print('size = ', size)
+        print("p_mat = ", rows_nparr)
+    return rows_nparr
